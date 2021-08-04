2a65ea23bbd54594d5be646ff8aba4f1edfe7a5e
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-08-04 13:55:28 -0600

    Add Trip property tests

--- src/Trip.js
+++ src/Trip.js
@@ -1,5 +1,6 @@
 class Trip {
   constructor(trip) {
+    //note you must get the trips arrays length (from fetch call) to assign a trip id.
     this.id = trip.id;
     this.userID = trip.userID;
     this.destinationID = trip.destinationID;
@@ -7,11 +8,19 @@ class Trip {
     this.date = trip.date;
     this.duration = trip.duration;
     this.status = trip.status;
-    this.suggestedActivities = trip.suggestedActivities;
+    this.suggestedActivities = trip.suggestedActivities || [];
   }
+
+  // THIS IS FORM VALIDATION, not class prop validation.
+  // SAD PATH: validate date test? Does this make sense to put on this class or outside
+  // Check against 0 travelers
+
+  //check date length... did you mean to put that you want to travel for a year?
 
 }
 
 export default Trip;
+
+//note you must get the trips arrays length (from fetch call) to assign a trip id.
 
 
--- test/Trip-test.js
+++ test/Trip-test.js
@@ -3,6 +3,20 @@ const expect = chai.expect;
 import Trip from '../src/Trip.js'
 
 describe('Trip', function() {
+  let trip;
+
+  before(() => {
+    trip = new Trip({
+      "id": 201,
+      "userID": 51,
+      "destinationID": 51,
+      "travelers": 2,
+      "date": "2022/10/16",
+      "duration": 6,
+      "status": "approved",
+      "suggestedActivities": ['skiing', 'hiking']
+    })
+  });
 
   it('should return true', function() {
     expect(true).to.equal(true);
@@ -11,6 +25,51 @@ describe('Trip', function() {
   it('should be a function', () => {
     expect(Trip).to.be.a('function');
   });
+
+  it('should be an instance of trip', () => {
+    expect(trip).to.be.an.instanceof(Trip);
+  }); 
+
+  it('should store an id', () => {
+    expect(trip.id).to.equal(201);
+  });
+
+  it('should store a user ID', () => {
+    expect(trip.userID).to.equal(51);
+  });
+
+  it('should store a destination ID', () => {
+    expect(trip.destinationID).to.equal(51);
+  });
+
+  it('should store a date', () => {
+    expect(trip.date).to.equal("2022/10/16");
+  });
+
+  it('should store a status', () => {
+    expect(trip.status).to.equal("approved");
+  });
+
+  it('should store no suggested activities as default', () => {
+
+    const trip2 = new Trip({
+      "id": 201,
+      "userID": 51,
+      "destinationID": 51,
+      "travelers": 2,
+      "date": "2022/10/16",
+      "duration": 6,
+      "status": "approved"
+    })
+
+    expect(trip2.suggestedActivities).to.deep.equal([]);
+
+  });
+
+  it('should store suggested activities', () => {
+    expect(trip.suggestedActivities).to.deep.equal(['skiing', 'hiking'])
+  });
+
 
 
 });
