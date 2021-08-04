39aec50c28f4c11ebdccce8ddc8da2b517cd733e
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-08-04 16:46:17 -0600

    Add  tests to check that instances of trips are being created in trips property

--- test/Agency-test.js
+++ test/Agency-test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 const expect = chai.expect;
 import Agency from '../src/Agency.js'
+import Trip from '../src/Trip.js';
 // import Trip from '../src/Trip.js'
 // import Destination from '../src/Destination.js'
 // import tripData from './test-data.js'
@@ -21,7 +22,24 @@ describe('Agency', function() {
     expect(agency).to.be.an.instanceof(Agency);
   }); 
 
-  // it('should store an id', () => {
+  it('should store a list of Trips', () => {
+    expect(agency.trips[0]).to.deep.equal(
+      new Trip( {
+        "id": 201,
+        "userID": 51,
+        "destinationID": 51,
+        "travelers": 2,
+        "date": "2022/10/16",
+        "duration": 6,
+        "status": "approved",
+        "suggestedActivities": ['skiing', 'hiking']
+      })
+    );
+
+    expect(agency.trips[0]).to.be.an.instanceof(Trip);
+  });
+
+  // it('should store a list of Destinations', () => {
   //   expect(trip.id).to.equal(201);
   // });
 
--- test/test-data.js
+++ test/test-data.js
@@ -4,7 +4,7 @@ import Agency from '../src/Agency.js';
 
 
 export const tripData = [
-  new Trip({
+  {
     "id": 201,
     "userID": 51,
     "destinationID": 51,
@@ -13,8 +13,8 @@ export const tripData = [
     "duration": 6,
     "status": "approved",
     "suggestedActivities": ['skiing', 'hiking']
-  }),
-  new Trip({
+  },
+  {
     "id": 202,
     "userID": 51,
     "destinationID": 51,
@@ -22,8 +22,8 @@ export const tripData = [
     "date": "2022/9/16",
     "duration": 6,
     "status": "approved",
-  }),
-  new Trip({
+  },
+  {
     "id": 203,
     "userID": 51,
     "destinationID": 51,
@@ -32,35 +32,35 @@ export const tripData = [
     "duration": 6,
     "status": "approved",
     "suggestedActivities": []
-  })
+  }
 ];
 
 
 export const destinationData = [
-  new Destination({
+  {
     "id": 51,
     "destination": "Chicago, Illinois",
     "estimatedLodgingCostPerDay": 60,
     "estimatedFlightCostPerPerson": 150,
     "image": "https://i.ibb.co/LPVszn3/dylan-lapierre-Rfeqh-K9-Bd-VI-unsplash.jpg",
     "alt": "overview of city buildings and a lakeshore"
-  }),
-  new Destination({
+  },
+  {
     "id": 52,
     "destination": "Pickerel Lake, Wisconsin",
     "estimatedLodgingCostPerDay": 150,
     "estimatedFlightCostPerPerson": 200,
     "image": "https://i.ibb.co/LPVszn3/dylan-lapierre-Rfeqh-K9-Bd-VI-unsplash.jpg",
     "alt": "paddleboat on lake with a sunset"
-  }),
-  new Destination({
+  },
+  {
     "id": 53,
     "destination": "Nederland, Colorado",
     "estimatedLodgingCostPerDay": 100,
     "estimatedFlightCostPerPerson": 60,
     "image": "https://i.ibb.co/LPVszn3/dylan-lapierre-Rfeqh-K9-Bd-VI-unsplash.jpg",
     "alt": "frozen man in a block of ice"
-  })
+  }
 ];
 
 export const agency = new Agency(tripData, destinationData);
