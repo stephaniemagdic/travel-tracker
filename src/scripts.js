2ec9bb3fb7b247dd52e6541b918aca4f3dbc13c5
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-08-05 15:56:31 -0600

    Add date logic to allow a user to select a start date on trip form

--- src/scripts.js
+++ src/scripts.js
@@ -10,4 +10,51 @@ import './images/turing-logo.png'
 console.log('This is the JavaScript entry file - your code begins here.');
 
 
+
+
 // global variables:  current User, date
+
+
+
+////////// GRAB THE FORM INPUT /////////////////////////
+
+// import dayjs from 'dayjs';
+// let defaultDate = new Date();
+// let currentDate = dayjs(defaultDate).format('YYYY/MM/DD');
+
+
+document.getElementById('book-a-trip-form').addEventListener('submit', (e) => {
+  requestTrip(e)
+});
+
+function requestTrip(e) {
+  //change the form min to todays date... on page reload.
+    // grab element attribute and set to todayDate.
+  const dateControl = document.querySelector('input[type="date"]');
+ 
+  e.preventDefault();
+  // const formData = new FormData(e.target);
+
+  const tripRequest = {
+    // date: formData.get('tripStart'),
+    // id: ,
+    // userID,
+    // destinationID,
+    // travelers: ,
+    date: formatDate(dateControl.value),
+    // duration: ,
+    // status: ,
+    // suggestedActivities: ,
+  }
+  console.log(tripRequest)
+}
+
+
+const formatDate = (dateToFormat) => {
+  const dividedDate = dateToFormat.split("-");
+  const month = dividedDate[1];
+  const day = dividedDate[2];
+  const year = dividedDate[0];
+  const rearrangedDate = [year, month, day];
+  return rearrangedDate.join("/");
+}
