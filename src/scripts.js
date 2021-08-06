33e1ccaf7c26db9749d588a8c2c19874c045c319
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-08-05 20:33:07 -0600

    Add logic for search bar filter to display only filtered desinations

--- src/scripts.js
+++ src/scripts.js
@@ -15,6 +15,47 @@ console.log('This is the JavaScript entry file - your code begins here.');
 // global variables:  current User, date
 
 
+//////////////EVENT LISTENERS/////////////
+window.addEventListener('load', loadPage);
+
+////////// FETCH REQUEST AND PAGE DISPLAY PAGE FUNCTION ///////////////
+
+// function loadPage() {
+//   Promise.resolve(fetchPageData()).then((data) => generateRepoClasses(data))
+//     .then(() => displayPageInfo());
+//     console.log("test")
+// }
+
+// function fetchPageData() {
+//   const userRepoPromise = fetchData('users') 
+//   const hydrationRepoPromise = fetchData('hydration')  
+//   const sleepRepoPromise = fetchData('sleep')  
+//   return Promise.all([userRepoPromise, hydrationRepoPromise, sleepRepoPromise]).then(values => values);
+// }
+
+// function generateRepoClasses(dataSets) {
+//   allUserData = new UserRepository(dataSets[0].userData);
+//   allHydrationData = new HydrationRepository(dataSets[1].hydrationData);
+//   allSleepData = new SleepRepository(dataSets[2].sleepData);
+// }
+
+// function fetchData(type) {
+//   return fetch(`http://localhost:3001/api/v1/${type}`)
+//     .then(response => response.json())
+//     .then(data => data)
+//     .catch(err => console.log(`ERROR with ${type}: ${err}`))
+// }
+
+//---------------------ALL DISPLAY FUNCTIONS----------------------------------//
+// function displayPageInfo() {
+//   const randomUser = allUserData.userData[Math.floor(Math.random()* allUserData.userData.length)]
+//   const user = new User(allUserData.returnUserData(randomUser.id));
+//   displayUserCard(user);
+//   displayAllHydrationData(user);
+//   displayAllSleepData(user);
+// }
+
+
 
 ////////// GRAB THE FORM INPUT /////////////////////////
 
@@ -22,6 +63,29 @@ console.log('This is the JavaScript entry file - your code begins here.');
 // let defaultDate = new Date();
 // let currentDate = dayjs(defaultDate).format('YYYY/MM/DD');
 
+
+/////////////// FORM SEARCH FUNCTIONALITY //////////////////
+//If no search criteria then show all**
+// searchBar.addEventListener('keyup', function(e) {
+//   createFilteredList(e);
+// });
+
+// const createFilteredList = (e) => {
+//   const searchedDestinations = e.target.value.toLowerCase();
+  
+//     let filteredDestinations = (AGENCYREPO.destinations).filter((destination) => {
+//       return (
+          // destination.name.toLowerCase().includes()  
+//       )
+//     });
+//     displayDestinations(filteredDestinations);
+ 
+// }
+
+
+
+
+///////BOOK A TRIP SUBMIT BUTTON /////////////////////
 
 document.getElementById('book-a-trip-form').addEventListener('submit', (e) => {
   requestTrip(e)
