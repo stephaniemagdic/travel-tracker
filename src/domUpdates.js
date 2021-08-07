921fcadec995f28031763e37c202d574f7a0699b
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-08-06 21:14:49 -0600

    Fix searchBar bug by resetting html each time function is called

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,9 +1,15 @@
 
 export const renderDestinations = (destinations) => {
   const destinationsContainer = document.getElementById('destinations-container') 
+  destinationsContainer.innerHTML = '';
   destinations.forEach(destination => {
     destinationsContainer.innerHTML += 
 
+    // create a card to display
+    // put in a carousel. 
+    // make sure they all have event listeners to be able to be selected by id 
+    // and populate the form with that city.
+    // then .find from all destinations based on that search.
 
     `<p>
       ${destination.id}
